bcd1f772d1dc4dad25fb4f80b6cdd51d394fc840
Jakub Karp <quni.inne@gmail.com>
2023-02-25 00:59:08 +0100

    SCSS structure and variables

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,4 @@
 /// Main JavaScript File
 /// Here we import all the global JavaScript files we need for our project.
 
-import "../global-styles/style.scss";
+import "../sass/pages/home-page.scss";
